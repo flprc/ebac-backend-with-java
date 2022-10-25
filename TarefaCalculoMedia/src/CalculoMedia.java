3a25c91c2699354908a0bce865f4b7096eb22f80
Felipe Cavalcante <103554466+flprc@users.noreply.github.com>
2022-10-25 19:53:40 -0300

    Add files via upload

---
+++ TarefaCalculoMedia/src/CalculoMedia.java
@@ -0,0 +1,11 @@
+public class CalculoMedia {
+
+    public static void main (String args []) {
+    int nota1 = 7;
+    int nota2 = 6;
+    int nota3 = 7;
+    int nota4 = 8;
+    int media = (nota1 + nota2 + nota3 + nota4) / 4;
+    System.out.println(media);
+    }
+}
